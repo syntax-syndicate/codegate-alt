1a9d2e6b34149e7212ee4c1d45c11dab1f56ecca
Luke Hinds <luke@stacklok.com>
2024-12-02 15:25:59 +0000

    Wire up secrets pipeline

--- src/codegate/server.py
+++ src/codegate/server.py
@@ -8,6 +8,8 @@ from codegate.pipeline.base import PipelineStep, SequentialPipelineProcessor
 from codegate.pipeline.codegate_context_retriever.codegate import CodegateContextRetriever
 from codegate.pipeline.codegate_system_prompt.codegate import CodegateSystemPrompt
 from codegate.pipeline.extract_snippets.extract_snippets import CodeSnippetExtractor
+from codegate.pipeline.secrets.signatures import CodegateSignatures
+from codegate.pipeline.secrets.secrets import CodegateSecrets
 from codegate.pipeline.version.version import CodegateVersion
 from codegate.providers.anthropic.provider import AnthropicProvider
 from codegate.providers.llamacpp.provider import LlamaCppProvider
@@ -29,7 +31,7 @@ def init_app() -> FastAPI:
         CodeSnippetExtractor(),
         CodegateSystemPrompt(Config.get_config().prompts.codegate_chat),
         CodegateContextRetriever(Config.get_config().prompts.codegate_chat),
-        # CodegateSecrets(),
+        CodegateSecrets(),
     ]
     # Leaving the pipeline empty for now
     fim_steps: List[PipelineStep] = []
@@ -40,7 +42,7 @@ def init_app() -> FastAPI:
     registry = ProviderRegistry(app)
 
     # Initialize SignaturesFinder
-    # CodegateSignatures.initialize("signatures.yaml")
+    CodegateSignatures.initialize("signatures.yaml")
 
     # Register all known providers
     registry.add_provider(
