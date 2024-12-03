6c3b13c8189216439561ad0bdbeed82bca715a1e
Alejandro Ponce <aponcedeleonch@stacklok.com>
2024-12-03 13:02:46 +0200

    added a call to ChatCompletionRequest in VLLM normalizer

--- src/codegate/providers/vllm/adapter.py
+++ src/codegate/providers/vllm/adapter.py
@@ -126,6 +126,8 @@ class VLLMInputNormalizer(ModelInputNormalizer):
         ret_data = normalized_data
         if self._has_chat_ml_format(normalized_data):
             ret_data = self._chat_ml_normalizer.normalize(normalized_data)
+        else:
+            ret_data = ChatCompletionRequest(**normalized_data)
         return ret_data
 
     def denormalize(self, data: ChatCompletionRequest) -> Dict:
