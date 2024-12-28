3a20bf59b1a2be360eaf4d2fd117be295d2dac04
Luke Hinds <luke@stacklok.com>
2024-12-28 17:24:29 +0000

    Cline Support
    This should be considered experiemental. I have tested it with
    anthropic and ollama (not all providers in cline allow a baseURL)
    
    The other that need validation is the OpenAI Compatible
    
    Some changes were needed to get Continue and Cline both capable
    in the same ollama provider, but all seems ok now

--- src/codegate/providers/ollama/provider.py
+++ src/codegate/providers/ollama/provider.py
@@ -46,11 +46,32 @@ class OllamaProvider(BaseProvider):
         Sets up Ollama API routes.
         """
 
-        # Special route for /api/tags that passes through directly
         @self.router.get(f"/{self.provider_route_name}/api/tags")
         async def get_tags(request: Request):
+            '''
+            Special route for /api/tags that responds outside of the pipeline
+            Tags are used to get the list of models
+            https://github.com/ollama/ollama/blob/main/docs/api.md#list-local-models
+            '''
             async with httpx.AsyncClient() as client:
                 response = await client.get(f"{self.base_url}/api/tags")
+                return response.json()
+
+        
+        @self.router.post(f"/{self.provider_route_name}/api/show")
+        async def show_model(request: Request):
+            '''
+            route for /api/show that responds outside of the pipeline
+            /api/show displays model is used to get the model information
+            https://github.com/ollama/ollama/blob/main/docs/api.md#show-model-information
+            '''
+            body = await request.body()
+            async with httpx.AsyncClient() as client:
+                response = await client.post(
+                    f"{self.base_url}/api/show",
+                    content=body,
+                    headers={"Content-Type": "application/json"}
+                )
                 return response.json()
 
         # Native Ollama API routes
