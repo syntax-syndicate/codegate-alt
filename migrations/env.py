34907a9e48466fccd2b0b693b7fad29472e24eb0
Alejandro Ponce <aponcedeleonch@stacklok.com>
2025-01-15 14:32:06 +0200

    Remove unuseful comments from alembic.ini

--- migrations/env.py
+++ migrations/env.py
@@ -5,11 +5,6 @@ from sqlalchemy import engine_from_config, pool
 # access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-# if config.config_file_name is not None:
-#     fileConfig(config.config_file_name)
-
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
