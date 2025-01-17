88b93e7ce6a33c7ca19943d1f2eebfd89b23f1bc
Alejandro Ponce <aponcedeleonch@stacklok.com>
2025-01-17 13:26:12 +0200

    Add `/api/v1` routes to openapi spec
    It was missing an import

--- tests/test_server.py
+++ tests/test_server.py
@@ -81,6 +81,7 @@ def test_health_check(test_client: TestClient) -> None:
     assert response.status_code == 200
     assert response.json() == {"status": "healthy"}
 
+
 @patch("codegate.dashboard.dashboard.fetch_latest_version", return_value="foo")
 def test_version_endpoint(mock_fetch_latest_version, test_client: TestClient) -> None:
     """Test the version endpoint."""
@@ -89,10 +90,11 @@ def test_version_endpoint(mock_fetch_latest_version, test_client: TestClient) ->
 
     response_data = response.json()
 
-    assert response_data["current_version"] == __version__.lstrip('v')
+    assert response_data["current_version"] == __version__.lstrip("v")
     assert response_data["latest_version"] == "foo"
     assert isinstance(response_data["is_latest"], bool)
     assert response_data["is_latest"] is False
+
 
 @patch("codegate.pipeline.secrets.manager.SecretsManager")
 @patch("codegate.server.ProviderRegistry")
