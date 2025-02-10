0753bd61b8f66a0adb0a80e27e1a37627b8d2195
Jakub Hrozek <jakub@stacklok.com>
2025-02-10 11:43:03 +0100

    Partial fixes for FIM with OpenRouter (#990)
    * Add openrouter integration tests
    
    A new provider need tests! Let's not be sloppy.
    
    * Workaround for litellm using a strict OpenAI provider for OpenRouter
    
    The problem is that litellm uses the OpenAI provider for talking to
    OpenRouter, but OpenRouter uses an OpenAI dialect - for example, the
    OpenAI python API no longer allows you to POST payloads that contain
    `post`, but those are often used with OpenRouter for FIM. The effect is
    that FIM payloads from Continue are rejected by litellm.
    
    To work around that, we add a FIM normalizer for OpenRouter that moves
    prompt to messages, like we often do during normalization, but in this
    normalizer we don't de-normalize but isntead pass on the payload with
    `messages` to the completion.
    
    * Listen on /openrouter/completions for FIM
    
    Continue sends FIM to openrouter to /completions, let's add that route
    
    * make lint

--- src/codegate/providers/openrouter/provider.py
+++ src/codegate/providers/openrouter/provider.py
@@ -1,16 +1,34 @@
 import json
+from typing import Dict
 
 from fastapi import Header, HTTPException, Request
+from litellm.types.llms.openai import ChatCompletionRequest
 
 from codegate.clients.detector import DetectClient
 from codegate.pipeline.factory import PipelineFactory
 from codegate.providers.fim_analyzer import FIMAnalyzer
+from codegate.providers.normalizer.completion import CompletionNormalizer
 from codegate.providers.openai import OpenAIProvider
+
+
+class OpenRouterNormalizer(CompletionNormalizer):
+    def __init__(self):
+        super().__init__()
+
+    def normalize(self, data: Dict) -> ChatCompletionRequest:
+        return super().normalize(data)
+
+    def denormalize(self, data: ChatCompletionRequest) -> Dict:
+        if data.get("had_prompt_before", False):
+            del data["had_prompt_before"]
+
+        return data
 
 
 class OpenRouterProvider(OpenAIProvider):
     def __init__(self, pipeline_factory: PipelineFactory):
         super().__init__(pipeline_factory)
+        self._fim_normalizer = OpenRouterNormalizer()
 
     @property
     def provider_route_name(self) -> str:
@@ -19,6 +37,7 @@ class OpenRouterProvider(OpenAIProvider):
     def _setup_routes(self):
         @self.router.post(f"/{self.provider_route_name}/api/v1/chat/completions")
         @self.router.post(f"/{self.provider_route_name}/chat/completions")
+        @self.router.post(f"/{self.provider_route_name}/completions")
         @DetectClient()
         async def create_completion(
             request: Request,
